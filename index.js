12eed5107be8305fa7aa92df3250f91c8be8ce5c
Martin Achtermann <mdacht777@gmail.com>
2020-10-24 13:09:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+// console.log("Wake Byron the poodle");
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
 }
 // leashDog
+// console.log("Leash Byron the poodle");
 function leashDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
 }
 // walkToPark
+// console.log("Walk to the park with Byron the poodle");
 function walkToPark(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 // throwFrisbee
+// console.log("Throw the frisbee for Byron the poodle");
 function throwFrisbee(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 // walkHome
+// console.log("Walk home with Byron the poodle");
 function walkHome(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
 }
 // unleashDog
+// console.log("Unleash Byron the poodle");
 function unleashDog(dogName, dogBreed) {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Unlease ${dogName} the ${dogBreed}`);
 }
